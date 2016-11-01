aeada8a0f2b62300c7aa5bde68dff6f245271d6d
ildar shafigullin <ildar66@inbox.ru>
2016-11-01 11:35:57 +0300

    add Introduction chapter. add Risks of Threads section

--- src/chapter_1/section_3/UnsafeSequence.java
+++ src/chapter_1/section_3/UnsafeSequence.java
@@ -5,6 +5,16 @@ import net.jcip.annotations.NotThreadSafe;
 /**
  * UnsafeSequence
  * @author Brian Goetz and Tim Peierls
+ *         Thread safety can be unexpectedly subtle because, in the absence of sufficient synchronization, the ordering of operations in multiple
+ *         threads is unpredictable and sometimes surprising. UnsafeSequence, which is supposed to generate a sequence of unique
+ *         integer values, offers a simple illustration of how the interleaving of actions in multiple threads can lead to undesirable results. It
+ *         behaves correctly in a single-threaded environment, but in a multithreaded environment does not
+ *
+ *         The problem with UnsafeSequence is that with some unlucky timing, two threads could call getNext and receive the same value. Figure 1.1
+ *         shows how this can happen. The increment notation, nextValue++, may appear to be a single operation, but is in fact three separate
+ *         operations: read the value, add one to it, and write out the new value. Since operations in multiple threads may be arbitrarily
+ *         interleaved by the runtime, it is possible for two threads to read the value at the same time, both see the same value, and then both add one to it.
+ *         The result is that the same sequence number is returned from multiple calls in different threads.
  */
 
 @NotThreadSafe
--- src/chapter_1/section_3/package-info.java
+++ src/chapter_1/section_3/package-info.java
@@ -1,5 +1,10 @@
 /**
  * Created by User on 01.11.2016.
  * Risks of Threads
+ * 
+ * Java's built-in support for threads is a double-edged sword. While it simplifies the development of concurrent applications by providing language
+ * and library support and a formal cross-platform memory model (it is this formal cross-platform memory model thatmakes possible the development of
+ * write-once, run-anywhere concurrent applications in Java), it also raises the bar for developers because more programs will use threads. When
+ * threads were more esoteric, concurrency was an "advanced" topic; now, mainstream developers must be aware of thread-safety issues.
  */
 package chapter_1.section_3;
