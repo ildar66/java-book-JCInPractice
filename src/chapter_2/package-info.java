91b39fb1bf307cc396e68a6ee83dc0db42c1984f
ildar shafigullin <ildar66@inbox.ru>
2016-11-01 12:17:24 +0300

    Thread Safety chapter.

--- src/chapter_2/package-info.java
+++ src/chapter_2/package-info.java
@@ -1,5 +1,15 @@
 /**
  * Created by User on 01.11.2016.
  * Thread Safety chapter
+ *
+ * If multiple threads access the same mutable state variable without appropriate synchronization, your program is broken.
+ * There are three ways to fix it:
+
+    Don't share the state variable across threads;
+
+    Make the state variable immutable; or
+
+    Use synchronization whenever accessing the state variable.
+
  */
 package chapter_2;
