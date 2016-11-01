de41a1f31b0a9fd6b858b24a9f19e9dd10e74271
ildar shafigullin <ildar66@inbox.ru>
2016-11-01 11:27:07 +0300

    add Introduction chapter. add Benefits of Threads section.

---
+++ src/chapter_1/section_2/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 01.11.2016.
+ * Benefits of Threads section.
+ * 
+ * When used properly, threads can reduce development and maintenance costs and improve the performance of complex applications. Threads make it
+ * easier to model how humans work and interact, by turning asynchronous workflows into mostly sequential ones. They can also turn otherwise
+ * convoluted code into straight-line code that is easier to write, read, and maintain.
+ * Threads are useful in GUI applications for improving the responsiveness of the user interface, and in server applications for improving resource
+ * utilization and throughput. They also simplify the implementation of the JVMthe garbage collector usually runs in one or more dedicated threads.
+ * Most nontrivial Java applications rely to some degree on threads for their organization.
+ */
+package chapter_1.section_2;
