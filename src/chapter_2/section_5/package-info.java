3640cb4c49bf853426c1710f1153e20c9e7e5441
ildar shafigullin <ildar66@inbox.ru>
2016-11-02 12:23:38 +0300

    Thread Safety chapter. Liveness and Performance. CachedFactorizer

--- src/chapter_2/section_5/package-info.java
+++ src/chapter_2/section_5/package-info.java
@@ -1,5 +1,10 @@
 /**
  * Created by User on 02.11.2016.
  * Liveness and Performance
+ *
+ * There is frequently a tension between simplicity and performance. When implementing a synchronization policy, resist the temptation to prematurely
+ * sacriflce simplicity (potentially compromising safety) for the sake of performance.
+ *
+ * Avoid holding locks during lengthy computations or operations at risk of not completing quickly such as network or console I/O.
  */
 package chapter_2.section_5;
