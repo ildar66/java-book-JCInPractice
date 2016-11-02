73c21720ed1a6c72cc6e196ab1a026e0ade34144
ildar shafigullin <ildar66@inbox.ru>
2016-11-02 10:34:54 +0300

    Thread Safety chapter. Example: A Stateless Servlet

---
+++ src/chapter_2/section_1/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 02.11.2016.
+ * What is Thread Safety?  section.
+ *
+ * A class is thread-safe if it behaves correctly when accessed from multiple threads, regardless of the scheduling or interleaving of the execution
+ * of those threads by the runtime environment, and with no additional synchronization or other coordination on the part of the calling code.
+ *
+ * Thread-safe classes encapsulate any needed synchronization so that clients need not provide their own.
+ */
+package chapter_2.section_1;
