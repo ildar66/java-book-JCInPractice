380baeab4034c49f2eab2eb78123677752e681a7
ildar shafigullin <ildar66@inbox.ru>
2016-11-02 11:11:35 +0300

    Thread Safety chapter. Atomicity section. Counting thread-safe Factorizer

--- src/chapter_2/section_2/package-info.java
+++ src/chapter_2/section_2/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Created by User on 02.11.2016.
  * Atomicity section.
+ *
+ * Operations A and B are atomic with respect to each other if, from the perspective of a thread executing A, when another thread executes B, either
+ * all of B has executed or none of it has. An atomic operation is one that is atomic with respect to all operations, including itself, that operate
+ * on the same state.
  */
 package chapter_2.section_2;
