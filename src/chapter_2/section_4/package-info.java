e9da3bda531c2a8dd007aaa0ac1f6eea7b1eb791
ildar shafigullin <ildar66@inbox.ru>
2016-11-02 12:04:35 +0300

    Thread Safety chapter. Guarding State with Locks section. Comment

--- src/chapter_2/section_4/package-info.java
+++ src/chapter_2/section_4/package-info.java
@@ -5,6 +5,15 @@
  * For each mutable state variable that may be accessed by more than one thread, all accesses to that variable must be performed with the same lock
  * held. In this case, we say that the variable is guarded by that lock.
  *
+ * There is no inherent relationship between an object's intrinsic lock and its state; an object's fields need not be guarded by its intrinsic lock,
+ * though this is a perfectly valid locking convention that is used by many classes. Acquiring the lock associated with an object does not prevent
+ * other threads from accessing that objectthe only thing that acquiring a lock prevents any other thread from doing is acquiring that same lock.
+ * The fact that every object has a built-in lock is just a convenience so that you needn't explicitly create lock objects.
+ * It is up to you to construct locking protocols or synchronization policies that let you access shared state safely,
+ * and to use them consistently throughout your program.
+ *
  * Every shared, mutable variable should be guarded by exactly one lock. Make it clear to maintainers which lock that is.
+ *
+ * For every invariant that involves more than one variable, all the variables involved in that invariant must be guarded by the same lock.
  */
 package chapter_2.section_4;
