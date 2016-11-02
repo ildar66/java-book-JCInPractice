9a987a0ae624b1eaef8495dcf7ebb3a7e4dac834
ildar shafigullin <ildar66@inbox.ru>
2016-11-02 11:49:57 +0300

    Thread Safety chapter. Guarding State with Locks section. Comment

--- src/chapter_2/section_4/package-info.java
+++ src/chapter_2/section_4/package-info.java
@@ -1,5 +1,10 @@
 /**
  * Created by User on 02.11.2016.
  * Guarding State with Locks section.
+ *
+ * For each mutable state variable that may be accessed by more than one thread, all accesses to that variable must be performed with the same lock
+ * held. In this case, we say that the variable is guarded by that lock.
+ *
+ * Every shared, mutable variable should be guarded by exactly one lock. Make it clear to maintainers which lock that is.
  */
 package chapter_2.section_4;
