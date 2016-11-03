7ba3d30402954c7466e7cd77963afb0b44c8e2eb
ildar shafigullin <ildar66@inbox.ru>
2016-11-03 11:55:21 +0300

    Sharing Objects chapter.Publication and Escape. UnsafeStates and Secrets.

--- src/chapter_3/section_2/Secrets.java
+++ src/chapter_3/section_2/Secrets.java
@@ -7,6 +7,8 @@ import java.util.Set;
  * Secrets
  * Publishing an object
  * @author Brian Goetz and Tim Peierls
+ *         The most blatant form of publication is to store a reference in a public static field, where any class and thread could see it.
+ *         The initialize method instantiates a new HashSet and publishes it by storing a reference to it into knownSecrets.
  */
 class Secrets {
 
