5e7b13a5599859da1d399aef969c4440ba242ffc
ildar shafigullin <ildar66@inbox.ru>
2016-11-03 11:09:00 +0300

    Sharing Objects chapter. Visibility section. Locking and Visibility comment

--- src/chapter_3/package-info.java
+++ src/chapter_3/package-info.java
@@ -1,5 +1,15 @@
 /**
  * Created by User on 02.11.2016.
  * Sharing Objects
+ *
+ * Locking and Visibility:
+ * 
+ * When thread A executes a synchronized block, and subsequently thread B enters a synchronized block guarded by the same lock, the values of
+ * variables that were visible to A prior to releasing the lock are guaranteed to be visible to B upon acquiring the lock. In other words,
+ * everything A did in or prior to a synchronized block is visible to B when it executes a synchronized block guarded by the same lock.
+ * Without synchronization, there is no such guarantee.
+ *
+ * Locking is not just about mutual exclusion; it is also about memory visibility. To ensure that all threads see the most up-to-date values of shared
+ * mutable variables, the reading and writing threads must synchronize on a common lock.
  */
 package chapter_3;
