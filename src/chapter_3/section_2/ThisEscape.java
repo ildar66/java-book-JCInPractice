8c27eddd32431248a5cfe1562a3c55d29711180d
ildar shafigullin <ildar66@inbox.ru>
2016-11-03 12:16:47 +0300

    Sharing Objects chapter.Publication and Escape. ThisEscape.

---
+++ src/chapter_3/section_2/ThisEscape.java
@@ -0,0 +1,38 @@
+package chapter_3.section_2;
+
+/**
+ * ThisEscape
+ * <p/>
+ * Implicitly allowing the this reference to escape
+ * @author Brian Goetz and Tim Peierls
+ *         When ThisEscape publishes the EventListener, it implicitly publishes the enclosing ThisEscape instance as well, because inner class
+ *         instances contain a hidden reference to the enclosing instance.
+ */
+public class ThisEscape {
+
+    public ThisEscape(EventSource source) {
+        source.registerListener(new EventListener() {
+
+            public void onEvent(Event e) {
+                doSomething(e);
+            }
+        });
+    }
+
+    void doSomething(Event e) {
+    }
+
+    interface EventSource {
+
+        void registerListener(EventListener e);
+    }
+
+    interface EventListener {
+
+        void onEvent(Event e);
+    }
+
+    interface Event {
+
+    }
+}
