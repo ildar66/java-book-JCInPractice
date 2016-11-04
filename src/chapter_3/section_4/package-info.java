475f6faf6ed30d213cd6826adb9e0fd8f465d240
ildar shafigullin <ildar66@inbox.ru>
2016-11-04 12:25:27 +0300

    Sharing Objects chapter.Immutability

---
+++ src/chapter_3/section_4/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 04.11.2016.
+ * Immutability
+ *
+ * Immutable objects are always thread-safe.
+ *
+ * An object is immutable if:
+ *      Its state cannot be modifled after construction;
+ *      All its flelds are final; and
+ *      It is properly constructed (the this reference does not escape during construction).
+ */
+package chapter_3.section_4;
