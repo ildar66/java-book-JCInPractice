eedd35e6a27cc8ee1a8c47bb146aba2acc250df4
ildar shafigullin <ildar66@inbox.ru>
2016-11-04 14:40:17 +0300

    Sharing Objects chapter.Safe Publication.Unsafe publication

--- src/chapter_3/section_5/StuffIntoPublic.java
+++ src/chapter_3/section_5/StuffIntoPublic.java
@@ -5,6 +5,8 @@ package chapter_3.section_5;
  * <p/>
  * Unsafe publication
  * @author Brian Goetz and Tim Peierls
+ *         Because of visibility problems, the Holder could appear to another thread to be in an inconsistent state, even though its invariants were
+ *         properly established by its constructor! This improper publication could allow another thread to observe a partially constructed object.
  */
 public class StuffIntoPublic {
 
