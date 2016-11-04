ed96ab028ac6565abfcc9a9ffae3ec8ffba1db97
ildar shafigullin <ildar66@inbox.ru>
2016-11-04 15:25:23 +0300

    Composing Objects chapter.

---
+++ src/chapter_4/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 04.11.2016.
+ * Composing Objects chapter.
+ *
+ * This chapter covers patterns for structuring classes that can make it easier to make them thread-safe and to maintain them without accidentally
+ * undermining their safety guarantees.
+ */
+package chapter_4;
