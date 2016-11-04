65cbda159092042e77e2706cdfd2f4d492c594d6
ildar shafigullin <ildar66@inbox.ru>
2016-11-04 11:48:18 +0300

    Sharing Objects chapter.Thread Confinement.Thread confinement of local primitive and reference variables

--- src/chapter_3/section_3/package-info.java
+++ src/chapter_3/section_3/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Created by User on 03.11.2016.
  * Thread Confinement section.
+ * 
+ * Accessing shared, mutable data requires using synchronization; one way to avoid this requirement is to not share. If data is only accessed from a
+ * single thread, no synchronization is needed. This technique, thread confinement, is one of the simplest ways to achieve thread safety. When an
+ * object is confined to a thread, such usage is automatically thread-safe even if the confined object itself is not
  */
 package chapter_3.section_3;
