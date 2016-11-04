1a73719098346fde2a957a72e2fa5d1c6eaa0772
ildar shafigullin <ildar66@inbox.ru>
2016-11-04 13:17:23 +0300

    Sharing Objects chapter.Safe Publication

---
+++ src/chapter_3/section_5/StuffIntoPublic.java
@@ -0,0 +1,16 @@
+package chapter_3.section_5;
+
+/**
+ * StuffIntoPublic
+ * <p/>
+ * Unsafe publication
+ * @author Brian Goetz and Tim Peierls
+ */
+public class StuffIntoPublic {
+
+    public Holder holder;
+
+    public void initialize() {
+        holder = new Holder(42);
+    }
+}
---
+++ src/chapter_3/section_5/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 04.11.2016.
+ * Safe Publication
+ * 
+ * So far we have focused on ensuring that an object not be published, such as when it is supposed to be confined to a thread or within another
+ * object. Of course, sometimes we do want to share objects across threads, and in this case we must do so safely.
+ */
+package chapter_3.section_5;
