c3e37e0ec3a2a5e88691f098573f118a62829f2b
ildar shafigullin <ildar66@inbox.ru>
2016-11-07 10:11:01 +0300

    Composing Objects chapter.Designing a Thread-safe Class.

---
+++ src/chapter_4/section_1/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * Created by User on 07.11.2016.
+ * Designing a Thread-safe Class
+ *
+ * The design process for a thread-safe class should include these three basic elements:
+
+     Identify the variables that form the object's state;
+
+     Identify the invariants that constrain the state variables;
+
+     Establish a policy for managing concurrent access to the object's state.
+
+ */
+package chapter_4.section_1;
+
+/*
+    You cannot ensure thread safety without understanding an object's invariants and postconditions.
+    Constraints on the valid values or state transitions for state variables can create atomicity and encapsulation requirements.
+*/
+
+
+
+
