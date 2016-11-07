b52b9d73600e361a56431d406a03858a0768460a
ildar shafigullin <ildar66@inbox.ru>
2016-11-07 11:18:10 +0300

    Composing Objects chapter. Confinement section.  Example: Tracking Fleet Vehicles

---
+++ src/chapter_4/section_2/MonitorVehicleTracker.java
@@ -0,0 +1,54 @@
+package chapter_4.section_2;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * MonitorVehicleTracker
+ * <p/>
+ * Monitor-based vehicle tracker implementation
+ * @author Brian Goetz and Tim Peierls
+ *         Even though MutablePoint is not thread-safe, the tracker class is. Neither the map nor any of the mutable points it contains is ever
+ *         published. When we need to a return vehicle locations to callers, the appropriate values are copied using either the MutablePoint copy
+ *         constructor or deepCopy, which creates a new Map whose values are copies of the keys and values from the old Map.[3]
+ */
+@ThreadSafe
+public class MonitorVehicleTracker {
+
+    @GuardedBy("this")
+    private final Map<String, MutablePoint> locations;
+
+    public MonitorVehicleTracker(Map<String, MutablePoint> locations) {
+        this.locations = deepCopy(locations);
+    }
+
+    public synchronized Map<String, MutablePoint> getLocations() {
+        return deepCopy(locations);
+    }
+
+    public synchronized MutablePoint getLocation(String id) {
+        MutablePoint loc = locations.get(id);
+        return loc == null ? null : new MutablePoint(loc);
+    }
+
+    public synchronized void setLocation(String id, int x, int y) {
+        MutablePoint loc = locations.get(id);
+        if (loc == null)
+            throw new IllegalArgumentException("No such ID: " + id);
+        loc.x = x;
+        loc.y = y;
+    }
+
+    private static Map<String, MutablePoint> deepCopy(Map<String, MutablePoint> m) {
+        Map<String, MutablePoint> result = new HashMap<String, MutablePoint>();
+
+        for (String id : m.keySet())
+            result.put(id, new MutablePoint(m.get(id)));
+
+        return Collections.unmodifiableMap(result);
+    }
+}
