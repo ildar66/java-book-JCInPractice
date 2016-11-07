18f276c133c35a97756350b5e20c1e602f29b6c9
ildar shafigullin <ildar66@inbox.ru>
2016-11-07 10:23:33 +0300

    Composing Objects chapter. Confinement section.

---
+++ src/chapter_4/section_2/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 07.11.2016.
+ * Instance Confinement section.
+ * 
+ * Encapsulating data within an object confines access to the data to the object's methods, making it easier to ensure that the data is always
+ * accessed with the appropriate lock held.
+ */
+package chapter_4.section_2;
