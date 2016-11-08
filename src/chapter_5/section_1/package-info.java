1fa5100a131f80d4e9e80ec9f27e1b755da9146f
ildar shafigullin <ildar66@inbox.ru>
2016-11-08 11:39:30 +0300

    Building Blocks chapter.Synchronized Collections section.Compound Actions on Vector Using Client-side Locking.

---
+++ src/chapter_5/section_1/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 08.11.2016.
+ * Synchronized Collections section.
+ *
+ * The synchronized collection classes include Vector and Hashtable, part of the original JDK, as well as their cousins added in JDK 1.2, the
+ * synchronized wrapper classes created by the Collections.synchronizedXxx factory methods. These classes achieve thread safety by encapsulating their
+ * state and synchronizing every public method so that only one thread at a time can access the collection state.
+ */
+package chapter_5.section_1;
