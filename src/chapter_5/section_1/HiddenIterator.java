71359a598e98d15ef02d40be9b77f85d081eacda
ildar shafigullin <ildar66@inbox.ru>
2016-11-08 12:05:27 +0300

    Building Blocks chapter. Concurrent Collections section.

--- src/chapter_5/section_1/HiddenIterator.java
+++ src/chapter_5/section_1/HiddenIterator.java
@@ -12,15 +12,15 @@ import net.jcip.annotations.GuardedBy;
  * Iteration hidden within string concatenation
  * @author Brian Goetz and Tim Peierls
  *         The addTenThings method could throw ConcurrentModificationException, because the collection is being iterated by toString in the process
- *         of
- *         preparing the debugging message. Of course, the real problem is that HiddenIterator is not thread-safe; the HiddenIterator lock should be
+ *         of preparing the debugging message. Of course, the real problem is that HiddenIterator is not thread-safe; the HiddenIterator lock should be
  *         acquired before using set in the println call, but debugging and logging code commonly neglect to do this.
+ *         
  *         The real lesson here is that the greater the distance between the state and the synchronization that guards it, the more likely that
  *         someone will forget to use proper synchronization when accessing that state. If HiddenIterator wrapped the HashSet with a synchronizedSet,
  *         encapsulating the synchronization, this sort of error would not occur.
  *         Just as encapsulating an object's state makes it easier to preserve its invariants, encapsulating its synchronization makes it easier to
  *         enforce its synchronization policy.
- *         
+ *
  *         Iteration is also indirectly invoked by the collection's hashCode and equals methods, which may be called if the collection is used as an
  *         element or key of another collection. Similarly, the containsAll, removeAll, and retainAll methods, as well as the constructors that take
  *         collections are arguments, also iterate the collection. All of these indirect uses of iteration can cause ConcurrentModificationException.
---
+++ src/chapter_5/section_2/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 08.11.2016.
+ * Concurrent Collections section.
+ */
+package chapter_5.section_2;
