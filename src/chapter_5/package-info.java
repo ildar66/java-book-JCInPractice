f4c001e147a033c540b1477eddbe1d0983f9f038
ildar shafigullin <ildar66@inbox.ru>
2016-11-08 11:24:30 +0300

    Building Blocks chapter.

---
+++ src/chapter_5/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Created by User on 08.11.2016.
+ * Building Blocks
+ *
+ * The last chapter explored several techniques for constructing thread-safe classes, including delegating thread safety to existing thread-safe
+ * classes. Where practical, delegation is one of the most effective strategies for creating thread-safe classes: just let existing thread-safe
+ * classes manage all the state.
+ * 
+ * The platform libraries include a rich set of concurrent building blocks, such as thread-safe collections and a variety of synchronizers that can
+ * coordinate the control flow of cooperating threads. This chapter covers the most useful concurrent building blocks, especially those introduced in
+ * Java 5.0 and Java 6, and some patterns for using them to structure concurrent applications.
+ */
+package chapter_5;
