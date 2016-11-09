2345f7bec8d8788ccd21ac74ec2f0499a1957b2a
ildar shafigullin <ildar66@inbox.ru>
2016-11-09 11:41:03 +0300

    Building Blocks chapter. Blocking and Interruptible Methods section. Restoring the Interrupted Status so as Not to Swallow the Interrupt.

---
+++ src/chapter_5/section_4/TaskRunnable.java
@@ -0,0 +1,31 @@
+package chapter_5.section_4;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * TaskRunnable
+ * <p/>
+ * Restoring the interrupted status so as not to swallow the interrupt
+ * @author Brian Goetz and Tim Peierls
+ */
+public class TaskRunnable implements Runnable {
+
+    BlockingQueue<Task> queue;
+
+    public void run() {
+        try {
+            processTask(queue.take());
+        } catch (InterruptedException e) {
+            // restore interrupted status
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    void processTask(Task task) {
+        // Handle the task
+    }
+
+    interface Task {
+
+    }
+}
--- src/chapter_5/section_4/package-info.java
+++ src/chapter_5/section_4/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Created by User on 09.11.2016.
  * Blocking and Interruptible Methods section.
- * 
+ *
  * When your code calls a method that throws InterruptedException, then your method is a blocking method too, and must have a plan for responding to
  * interruption. For library code, there are basically two choices:
      *  Propagate the InterruptedException. This is often the most sensible policy if you can get away with itjust propagate the InterruptedException to
@@ -11,5 +11,11 @@
      *  Restore the interrupt. Sometimes you cannot throw InterruptedException, for instance when your code is part of a Runnable. In these situations, you
         must catch InterruptedException and restore the interrupted status by calling interrupt on the current thread, so that code higher up the call
         stack can see that an interrupt was issued, as demonstrated in Listing 5.10.
- */
+ *
+    You can get much more sophisticated with interruption, but these two approaches should work in the vast majority of situations.
+    But there is one thing you should not do with InterruptedExceptioncatch it and do nothing in response.
+    This deprives code higher up on the call stack of the opportunity to act on the interruption, because the evidence that the thread was interrupted
+    is lost. The only situation in which it is acceptable to swallow an interrupt is when you are extending Thread and therefore control all the codehigher
+    up on the call stack.
+*/
 package chapter_5.section_4;
