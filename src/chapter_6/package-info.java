c4c0ed6ce488ac504f2792d3bc290dfdaf6b3714
ildar shafigullin <ildar66@inbox.ru>
2016-11-10 12:31:02 +0300

    Task Execution chapter.

---
+++ src/chapter_6/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 10.11.2016.
+ * Task Execution chapter
+ *
+ * Most concurrent applications are organized around the execution of tasks: abstract, discrete units of work.
+ * Dividing the work of an application into tasks simplifies program organization,
+ * facilitates error recovery by providing natural transaction boundaries,
+ * and promotes concurrency by providing a natural structure for parallelizing work.
+ */
+package chapter_6;
