b8503783fdb9ac8dc9c5e692e11c26d37fcaede5
ildar shafigullin <ildar66@inbox.ru>
2016-11-15 13:17:30 +0300

    Cancellation and Shutdown chapter. Handling Abnormal Thread Termination section. UncaughtExceptionHandler that logs the exception
    .

--- src/chapter_7/section_3/package-info.java
+++ src/chapter_7/section_3/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Created by User on 15.11.2016.
  * Handling Abnormal Thread Termination section.
+ * 
+ * In long-running applications, always use uncaught exception handlers for all threads that at least log the exception.
  */
 package chapter_7.section_3;
