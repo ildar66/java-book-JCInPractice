7a52a93c5e45b8e0d5804007e1cf5abb84350aae
ildar shafigullin <ildar66@inbox.ru>
2016-11-16 12:44:27 +0300

    Applying Thread Pools chapter. Configuring ThreadPoolExecutor section.Thread Factories

---
+++ src/chapter_8/section_3/MyThreadFactory.java
@@ -0,0 +1,22 @@
+package chapter_8.section_3;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * MyThreadFactory
+ * <p/>
+ * Custom thread factory
+ * @author Brian Goetz and Tim Peierls
+ */
+public class MyThreadFactory implements ThreadFactory {
+
+    private final String poolName;
+
+    public MyThreadFactory(String poolName) {
+        this.poolName = poolName;
+    }
+
+    public Thread newThread(Runnable runnable) {
+        return new MyAppThread(runnable, poolName);
+    }
+}
