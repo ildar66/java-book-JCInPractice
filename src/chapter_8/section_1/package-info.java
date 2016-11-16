f53d885a1b89d8827e6ec43ea8f6a3630c6ef805
ildar shafigullin <ildar66@inbox.ru>
2016-11-16 11:42:22 +0300

    Applying Thread Pools chapter. Implicit Couplings Between Tasks and Execution Policies section.Task that deadlocks in a single-threaded Executor

--- src/chapter_8/section_1/package-info.java
+++ src/chapter_8/section_1/package-info.java
@@ -1,5 +1,12 @@
 /**
  * Created by User on 15.11.2016.
  * Implicit Couplings Between Tasks and Execution Policies section.
+ *
+ * Some tasks have characteristics that require or preclude a specific execution policy. Tasks that depend on other tasks require that the thread
+ * pool be large enough that tasks are never queued or rejected; tasks that exploit thread confinement require sequential execution. Document these
+ * requirements so that future maintainers do not undermine safety or liveness by substituting an incompatible execution policy.
+ * 
+ * Whenever you submit to an Executor tasks that are not independent, be aware of the possibility of thread starvation deadlock, and document any pool
+ * sizing or configuration constraints in the code or configuration file where the Executor is configured.
  */
 package chapter_8.section_1;
