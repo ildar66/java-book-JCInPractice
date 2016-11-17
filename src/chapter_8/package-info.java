dc856bd1db221dc067dfb89a1872bbc9f10366c2
ildar shafigullin <ildar66@inbox.ru>
2016-11-17 12:36:32 +0300

    Applying Thread Pools chapter.comment Summary

--- src/chapter_8/package-info.java
+++ src/chapter_8/package-info.java
@@ -1,5 +1,9 @@
 /**
  * Created by User on 15.11.2016.
  * Applying Thread Pools chapter.
+ * The Executor framework is a powerful and flexible framework for concurrently executing tasks. It offers a number of tuning options, such as
+ * policies for creating and tearing down threads, handling queued tasks, and what to do with excess tasks, and provides several hooks for extending
+ * its behavior. As in most powerful frameworks, however, there are combinations of settings that do not work well together; some types of tasks
+ * require specific execution policies, and some combinations of tuning parameters may produce strange results.
  */
 package chapter_8;
