e570f1d6c5d3de9d2e8e194e24dc0490de5cc63a
ildar shafigullin <ildar66@inbox.ru>
2016-11-17 12:31:21 +0300

    Applying Thread Pools chapter. Parallelizing Recursive Algorithms section. ConcurrentPuzzleSolver

---
+++ src/chapter_8/section_5/ValueLatch.java
@@ -0,0 +1,38 @@
+package chapter_8.section_5;
+
+import java.util.concurrent.CountDownLatch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * ValueLatch
+ * <p/>
+ * Result-bearing latch used by ConcurrentPuzzleSolver
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class ValueLatch<T> {
+
+    @GuardedBy("this")
+    private T value = null;
+    private final CountDownLatch done = new CountDownLatch(1);
+
+    public boolean isSet() {
+        return (done.getCount() == 0);
+    }
+
+    public synchronized void setValue(T newValue) {
+        if (!isSet()) {
+            value = newValue;
+            done.countDown();
+        }
+    }
+
+    public T getValue() throws InterruptedException {
+        done.await();
+        synchronized (this) {
+            return value;
+        }
+    }
+}
