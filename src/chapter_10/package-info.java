6fa1269fa0a8e809f234c97742cf64e98f12d4c2
ildar shafigullin <ildar66@inbox.ru>
2016-11-18 14:34:10 +0300

    Avoiding Liveness Hazards chapter. Deadlock section.

---
+++ src/chapter_10/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 18.11.2016.
+ * Avoiding Liveness Hazards chapter.
+ */
+package chapter_10;
---
+++ src/chapter_10/section_1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 18.11.2016.
+ * Deadlock section.
+ */
+package chapter_10.section_1;
