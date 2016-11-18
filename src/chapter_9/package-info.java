8ec48f7be053b25217b3bfcb8e867b7fc3728d1d
ildar shafigullin <ildar66@inbox.ru>
2016-11-18 14:30:51 +0300

    GUI Applications chapter summary comment

--- src/chapter_9/package-info.java
+++ src/chapter_9/package-info.java
@@ -1,5 +1,14 @@
 /**
  * Created by User on 17.11.2016.
  * GUI Applications chapter.
+ *
+ * Consider a split-model design when a data model must be shared by more than one thread and implementing a thread-safe data model would be
+ * inadvisable because of blocking, consistency, or complexity reasons.
+ *
+ * GUI frameworks are nearly always implemented as single-threaded subsystems in which all presentation-related code runs as tasks in an event thread.
+ * Because there is only a single event thread, long-running tasks can compromise responsiveness and so should be executed in background threads.
+ * Helper classes like SwingWorker or the BackgroundTask class built here, which provide support for cancellation, progress indication,
+ * and completion indication, can simplify the development of long-running tasks that have both GUI and non-GUI components.
+
  */
 package chapter_9;
