f3c1f119555ed29d1f1e9e1d193d9273ceb36868
ildar shafigullin <ildar66@inbox.ru>
2016-11-21 12:03:40 +0300

    Avoiding Liveness Hazards chapter summary.

--- src/chapter_10/package-info.java
+++ src/chapter_10/package-info.java
@@ -1,5 +1,13 @@
 /**
  * Created by User on 18.11.2016.
  * Avoiding Liveness Hazards chapter.
+ *
+ * Avoid the temptation to use thread priorities, since they increase platform dependence and can cause liveness problems.
+ * Most concurrent applications can use the default priority for all threads.
+ *
+ * Liveness failures are a serious problem because there is no way to recover from them short of aborting the application.
+ * The most common form of liveness failure is lock-ordering deadlock. Avoiding lock ordering deadlock starts at design time:
+ * ensure that when threads acquire multiple locks, they do so in a consistent order. The best way to do this is by using open calls throughout
+ * your program. This greatly reduces the number of places where multiple locks are held at once, and makes it more obvious where those places are.
  */
 package chapter_10;
--- src/chapter_10/section_2/package-info.java
+++ src/chapter_10/section_2/package-info.java
@@ -1,6 +1,13 @@
 /**
  * Created by User on 21.11.2016.
- * 
  * Avoiding and Diagnosing Deadlocks section.
+ * 
+ * A program that never acquires more than one lock at a time cannot experience lock-ordering deadlock. Of course, this is not always practical, but
+ * if you can get away with it, it's a lot less work. If you must acquire multiple locks, lock ordering must be a part of your design: try to minimize
+ * the number of potential locking interactions, and follow and document a lock-ordering protocol for locks that may be acquired together.
+ * In programs that use fine-grained locking, audit your code for deadlock freedom using a two-part strategy: first, identify where multiple locks
+ * could be acquired (try to make this a small set), and then perform a global analysis of all such instances to ensure that lock ordering is
+ * consistent across your entire program. Using open calls wherever possible simplifies this analysis substantially. With no non-open calls, finding
+ * instances where multiple locks are acquired is fairly easy, either by code review or by automated bytecode or source code analysis.
  */
 package chapter_10.section_2;
