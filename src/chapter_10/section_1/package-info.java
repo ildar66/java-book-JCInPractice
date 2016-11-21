682f14604e3043e768422c5893f8d060e847119d
ildar shafigullin <ildar66@inbox.ru>
2016-11-21 10:23:25 +0300

    Avoiding Liveness Hazards chapter. Deadlock section. Dynamic lock-ordering deadlock

--- src/chapter_10/section_1/package-info.java
+++ src/chapter_10/section_1/package-info.java
@@ -1,5 +1,6 @@
 /**
  * Created by User on 18.11.2016.
  * Deadlock section.
+ * A program will be free of lock-ordering deadlocks if all threads acquire the locks they need in a fixed global order.
  */
 package chapter_10.section_1;
