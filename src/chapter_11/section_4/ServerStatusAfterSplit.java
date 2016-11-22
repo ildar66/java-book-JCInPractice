800ab7b4f53f83757037889516fd65359bca948f
ildar shafigullin <ildar66@inbox.ru>
2016-11-22 11:49:04 +0300

    Performance and Scalability chapter. Reducing Lock Contention section. Refactored to Use Split Locks.

---
+++ src/chapter_11/section_4/ServerStatusAfterSplit.java
@@ -0,0 +1,51 @@
+package chapter_11.section_4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * ServerStatusAfterSplit
+ * <p/>
+ * ServerStatus refactored to use split locks
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class ServerStatusAfterSplit {
+
+    @GuardedBy("users")
+    public final Set<String> users;
+    @GuardedBy("queries")
+    public final Set<String> queries;
+
+    public ServerStatusAfterSplit() {
+        users = new HashSet<String>();
+        queries = new HashSet<String>();
+    }
+
+    public void addUser(String u) {
+        synchronized (users) {
+            users.add(u);
+        }
+    }
+
+    public void addQuery(String q) {
+        synchronized (queries) {
+            queries.add(q);
+        }
+    }
+
+    public void removeUser(String u) {
+        synchronized (users) {
+            users.remove(u);
+        }
+    }
+
+    public void removeQuery(String q) {
+        synchronized (queries) {
+            queries.remove(q);
+        }
+    }
+}
