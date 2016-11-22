653ff0e03cc872d00f43b5770979a2977a0b0fc2
ildar shafigullin <ildar66@inbox.ru>
2016-11-22 11:27:01 +0300

    Performance and Scalability chapter. Costs Introduced by Threads section.

--- src/chapter_11/section_3/package-info.java
+++ src/chapter_11/section_3/package-info.java
@@ -1,5 +1,8 @@
 /**
  * Created by User on 22.11.2016.
  * Costs Introduced by Threads section.
+ * 
+ * Don't worry excessively about the cost of uncontended synchronization. The basic mechanism is already quite fast, and JVMs can perform additional
+ * optimizations that further reduce or eliminate the cost. Instead, focus optimization efforts on areas where lock contention actually occurs.
  */
 package chapter_11.section_3;
