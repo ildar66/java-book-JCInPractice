bfa19cd431d70cf68e63c140d68ea1f43ac6eb04
ildar shafigullin <ildar66@inbox.ru>
2016-11-23 11:08:20 +0300

    Testing Concurrent Programs chapter. Testing for Correctness section.

---
+++ src/chapter_12/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 23.11.2016.
+ * Testing Concurrent Programs chapter.
+ * Related to liveness tests are performance tests. Performance can be measured in a number of ways, including:
+ * Throughput: the rate at which a set of concurrent tasks is completed;
+ * Responsiveness: the delay between a request for and completion of some action (also called latency); or
+ * Scalability: the improvement in throughput (or lack thereof) as more resources (usually CPUs) are made available.
+ */
+package chapter_12;
---
+++ src/chapter_12/section_1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 23.11.2016.
+ * Testing for Correctness section.
+ */
+package chapter_12.section_1;
