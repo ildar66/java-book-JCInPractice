e60ba6b1f3f74b7b6f73c80ce8e228294ed374de
ildar shafigullin <ildar66@inbox.ru>
2016-11-24 10:50:18 +0300

    Testing Concurrent Programs chapter. Avoiding Performance Testing Pitfalls section.

---
+++ src/chapter_12/section_3/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by User on 24.11.2016.
+ * Avoiding Performance Testing Pitfalls
+ * Writing effective performance tests requires tricking the optimizer into not optimizing away your benchmark as dead code. This requires every
+ * computed result to be used somehow by your programin a way that does not require synchronization or substantial computation.
+ */
+package chapter_12.section_3;
