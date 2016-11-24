99dd4ad4b1b96b549b954d1081687b65c5637560
ildar shafigullin <ildar66@inbox.ru>
2016-11-24 11:52:21 +0300

    Explicit Locks chapter.Lock and ReentrantLock section.

---
+++ src/chapter_13/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 24.11.2016.
+ * Explicit Locks chapter.
+ * Before Java 5.0, the only mechanisms for coordinating access to shared data were synchronized and volatile. Java 5.0 adds another option:
+ * ReentrantLock. Contrary to what some have written, ReentrantLock is not a replacement for intrinsic locking, but rather an alternative with
+ * advanced features for when intrinsic locking proves too limited.
+ */
+package chapter_13;
