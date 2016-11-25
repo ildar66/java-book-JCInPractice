5178ed9d2905502101f1442848139be5fb2f3824
ildar shafigullin <ildar66@inbox.ru>
2016-11-25 11:44:38 +0300

    Explicit Locks chapter.Choosing Between Synchronized and ReentrantLock section.

---
+++ src/chapter_13/section_4/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 25.11.2016.
+ * Choosing Between Synchronized and ReentrantLock section.
+ * 
+ * ReentrantLock is an advanced tool for situations where intrinsic locking is not practical. Use it if you need its advanced features: timed, polled,
+ * or interruptible lock acquisition, fair queueing, or non-block-structured locking. Otherwise, prefer synchronized.
+ */
+package chapter_13.section_4;
