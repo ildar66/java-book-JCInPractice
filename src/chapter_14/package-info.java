111cf8a39b559478efe51b0f2dadc68e6d2937b6
ildar shafigullin <ildar66@inbox.ru>
2016-11-25 11:57:56 +0300

    Building Custom Synchronizers chapter.Managing State Dependence section.

---
+++ src/chapter_14/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 25.11.2016.
+ * Building Custom Synchronizers chapter.
+ */
+package chapter_14;
---
+++ src/chapter_14/section_1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 25.11.2016.
+ * Managing State Dependence section.
+ */
+package chapter_14.section_1;
