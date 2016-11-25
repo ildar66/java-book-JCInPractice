ecbe6ccc393072a6a7a3f9e02182b4da1f062fc3
ildar shafigullin <ildar66@inbox.ru>
2016-11-25 12:11:01 +0300

    Building Custom Synchronizers chapter.Managing State Dependence section.Bounded buffer using crude blocking

--- src/chapter_14/section_1/SleepyBoundedBuffer.java
+++ src/chapter_14/section_1/SleepyBoundedBuffer.java
@@ -9,10 +9,12 @@ import net.jcip.annotations.ThreadSafe;
  * Bounded buffer using crude blocking
  * @author Brian Goetz and Tim Peierls
  *         SleepyBoundedBuffer in Listing attempts to spare callers the inconvenience of implementing the retry logic on each call by
- *         encapsulating the same crude "poll and sleep" retry mechanism within the put and take operations. If the buffer is empty, take sleeps until
+ *         encapsulating the same crude "poll and sleep" retry mechanism within the put and take operations. If the buffer is empty, take sleeps
+ *         until
  *         another thread puts some data into the buffer; if the buffer is full, put sleeps until another thread makes room by removing some data.
  *         This approach encapsulates precondition management and simplifies using the bufferdefinitely a step in the right direction.
- *         The implementation of SleepyBoundedBuffer is more complicated than the previous attempt.[3] The buffer code must test the appropriate state
+ *         The implementation of SleepyBoundedBuffer is more complicated than the previous attempt.[3] The buffer code must test the appropriate
+ *         state
  *         condition with the buffer lock held, because the variables that represent the state condition are guarded by the buffer lock. If the test
  *         fails, the executing thread sleeps for a while, first releasing the lock so other threads can access the buffer.[4] Once the thread wakes
  *         up, it reacquires the lock and tries again, alternating between sleeping and testing the state condition until the operation can proceed.
