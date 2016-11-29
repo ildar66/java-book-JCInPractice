7d36434fddd20fa91224ba98190419db41458a89
ildar shafigullin <ildar66@inbox.ru>
2016-11-29 11:05:59 +0300

    Atomic Variables and Nonblocking Synchronization chapter.Disadvantages of Locking section.

--- src/chapter_15/package-info.java
+++ src/chapter_15/package-info.java
@@ -1,5 +1,13 @@
 /**
  * Created by User on 28.11.2016.
  * Atomic Variables and Nonblocking Synchronization chapter.
+ *
+ * Many of the classes in java.util.concurrent, such as Semaphore and ConcurrentLinkedQueue, provide better performance and scalability than
+ * alternatives using synchronized. In this chapter, we take a look at the primary source of this performance boost: atomic variables and nonblocking
+ * synchronization.
+ * 
+ * Much of the recent research on concurrent algorithms has focused on nonblocking algorithms, which use low-level atomic machine instructions such
+ * as compare-and-swap instead of locks to ensure data integrity under concurrent access. Nonblocking algorithms are used extensively in operating
+ * systems and JVMs for thread and process scheduling, garbage collection, and to implement locks and other concurrent data structures.
  */
 package chapter_15;
---
+++ src/chapter_15/section_1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 29.11.2016.
+ * Disadvantages of Locking section.
+ */
+package chapter_15.section_1;
