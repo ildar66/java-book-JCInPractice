638e5d6fe349b397680ab6f666cd5278534b1abb
ildar shafigullin <ildar66@inbox.ru>
2016-11-29 12:35:47 +0300

    Atomic Variables and Nonblocking Synchronization chapter.Nonblocking Algorithms section.

--- src/chapter_15/section_3/package-info.java
+++ src/chapter_15/section_3/package-info.java
@@ -1,5 +1,10 @@
 /**
  * Created by User on 29.11.2016.
  * Atomic Variable Classes section.
+ * 
+ * The performance reversal between locks and atomics at differing levels of contention illustrates the strengths and weaknesses of each. With low to
+ * moderate contention, atomics offer better scalability; with high contention, locks offer better contention avoidance. (CAS-based algorithms also
+ * outperform lock-based ones on single-CPU systems, since a CAS always succeeds on a single-CPU system except in the unlikely case that a thread is
+ * preempted in the middle of the read-modify-write operation.)
  */
 package chapter_15.section_3;
---
+++ src/chapter_15/section_4/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 29.11.2016.
+ * Nonblocking Algorithms section.
+ */
+package chapter_15.section_4;
