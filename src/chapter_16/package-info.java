4f340025979c6d4a303ba47c80ab41dff41991d5
ildar shafigullin <ildar66@inbox.ru>
2016-11-30 12:28:19 +0300

    The Java Memory Model chapter.What is a Memory Model, and Why would I Want One?

---
+++ src/chapter_16/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 30.11.2016.
+ * The Java Memory Model chapter.
+ */
+package chapter_16;
---
+++ src/chapter_16/section_1/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by User on 30.11.2016.
+ * What is a Memory Model, and Why would I Want One?
+ */
+package chapter_16.section_1;
