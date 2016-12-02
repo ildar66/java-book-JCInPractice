4effc7525e203b27eb30fe96a7fd43c344285a00
ildar shafigullin <ildar66@inbox.ru>
2016-12-02 11:58:19 +0300

    The Java Memory Model chapter.Publication section. SafeLazyInitialization

---
+++ src/chapter_16/section_2/SafeLazyInitialization.java
@@ -0,0 +1,25 @@
+package chapter_16.section_2;
+
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * SafeLazyInitialization
+ * <p/>
+ * Thread-safe lazy initialization
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class SafeLazyInitialization {
+
+    private static Resource resource;
+
+    public synchronized static Resource getInstance() {
+        if (resource == null)
+            resource = new Resource();
+        return resource;
+    }
+
+    static class Resource {
+
+    }
+}
--- src/chapter_16/section_2/package-info.java
+++ src/chapter_16/section_2/package-info.java
@@ -1,5 +1,8 @@
 /**
  * Created by User on 02.12.2016.
  * Publication section.
+ *
+ * With the exception of immutable objects, it is not safe to use an object that has been initialized by another thread unless the publication
+ * happens-before the consuming thread uses it.
  */
 package chapter_16.section_2;
